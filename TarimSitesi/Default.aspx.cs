f312102fa9911749c4d1aebccd7bc05df6a06440
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-13 17:40:35 +0300

    Add project files.

---
+++ TarimSitesi/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TarimSitesi
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TblBilgiTableAdapter dt= new DataSet1TableAdapters.TblBilgiTableAdapter();
+            Repeater1.DataSource = dt.BilgiGetir();
+            Repeater1.DataBind();
+
+            DataSet1TableAdapters.TblUrunlerTableAdapter dt2 = new DataSet1TableAdapters.TblUrunlerTableAdapter();
+            Repeater2.DataSource = dt2.UrunGetir();
+            Repeater2.DataBind();
+
+        }
+    }
+}
